0c2ead880fc445ab403b29e64054269bad659e33
Wanqj <wqj97@126.com>
2017-05-23 18:35:03 +0800

    2017-05-23

--- kmeans.py
+++ kmeans.py
@@ -4,19 +4,44 @@ from random import choice, shuffle
 from numpy import array
 import matplotlib.pyplot as plt
 import numpy as np
-
+from matplotlib.colors import ListedColormap
 from sklearn.datasets.samples_generator import make_blobs
+from sklearn.datasets.samples_generator import make_circles
 
-# from sklearn.datasets.samples_generator import make_circles
 
-K = 4  # 类别数目
-MAX_ITERS = 1000  # 最大迭代次数
-N = 200  # 样本点数目
+def ScatterPlot(X, Y, assignments=None, centers=None):
+    if assignments is None:
+        assignments = [0] * len(X)
+    fig = plt.figure(figsize=(14, 8))
+    cmap = ListedColormap(['red', 'green', 'blue', 'magenta'])
+    plt.scatter(X, Y, c=assignments, cmap=cmap)
+    if centers is not None:
+        plt.scatter(centers[:, 0], centers[:, 1], c=range(len(centers)),
+                    marker='+', s=400, cmap=cmap)
+    plt.xlabel('Height (in)')
+    plt.ylabel('Weight (lbs)')
+    plt.show()
+
 
 centers = [[-2, -2], [-2, 1.5], [1.5, -2], [2, 1.5]]  # 簇中心
 
 # 生成人工数据集
 # data, features = make_circles(n_samples=200, shuffle=True, noise=0.1, factor=0.4)
-data, features = make_blobs(n_samples=N, centers=centers, n_features=2, cluster_std=0.8, shuffle=False, random_state=42)
+data, features = make_blobs(n_samples=200, centers=centers, n_features=2, cluster_std=0.8, shuffle=False, random_state=42)
+
+# 显示原图像
 plt.scatter(data[:, 0], data[:, 1])
-plt.show()
+# plt.show()
+
+kmeans = tf.contrib.learn.KMeansClustering(num_clusters=4, relative_tolerance=0.0001)
+
+sess = tf.Session()
+def input_fn():
+    return tf.constant(data, tf.float32, np.shape(data)), None
+
+kmeans.fit(input_fn=input_fn)
+clusters = kmeans.clusters()
+print clusters
+assignments = list(kmeans.predict_cluster_idx(input_fn=input_fn))
+print assignments
+ScatterPlot(data[:, 0], data[:, 1], assignments, clusters)
