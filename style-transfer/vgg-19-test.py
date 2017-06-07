46309e13ce37a20161f2c53024253198ea244528
Wanqj <wqj97@126.com>
2017-06-07 17:16:50 +0800

    update readme 2017-06-07

--- style-transfer/vgg-19-test.py
+++ style-transfer/vgg-19-test.py
@@ -1,5 +1,5 @@
 import scipy.io as io
 
-vggnet = io.loadmat('./imagenet-vgg-verydeep-19.mat')
+vggnet = io.loadmat('fast-style-transfer-master/data/imagenet-vgg-verydeep-19.mat')
 
-print vggnet['layers']
+print vggnet
