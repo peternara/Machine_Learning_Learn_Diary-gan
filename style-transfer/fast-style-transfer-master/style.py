7811d541ba90c4f27cae94eafcfab81c87ffad1c
Wanqj <wqj97@126.com>
2017-06-08 23:10:15 +0800

    update new style 2017-06-08

--- style-transfer/fast-style-transfer-master/style.py
+++ style-transfer/fast-style-transfer-master/style.py
@@ -1,7 +1,9 @@
+# coding=utf-8
 from __future__ import print_function
 import sys, os, pdb
+
 sys.path.insert(0, 'src')
-import numpy as np, scipy.misc 
+import numpy as np, scipy.misc
 from src import optimize
 from argparse import ArgumentParser
 from src.utils import save_img, get_img, exists, list_files
@@ -20,6 +22,7 @@ TRAIN_PATH = 'data/train2014'
 BATCH_SIZE = 4
 DEVICE = '/gpu:0'
 FRAC_GPU = 1
+
 
 def build_parser():
     parser = ArgumentParser()
@@ -69,7 +72,7 @@ def build_parser():
                         dest='content_weight',
                         help='content weight (default %(default)s)',
                         metavar='CONTENT_WEIGHT', default=CONTENT_WEIGHT)
-    
+
     parser.add_argument('--style-weight', type=float,
                         dest='style_weight',
                         help='style weight (default %(default)s)',
@@ -79,13 +82,14 @@ def build_parser():
                         dest='tv_weight',
                         help='total variation regularization weight (default %(default)s)',
                         metavar='TV_WEIGHT', default=TV_WEIGHT)
-    
+
     parser.add_argument('--learning-rate', type=float,
                         dest='learning_rate',
                         help='learning rate (default %(default)s)',
                         metavar='LEARNING_RATE', default=LEARNING_RATE)
 
     return parser
+
 
 def check_opts(opts):
     exists(opts.checkpoint_dir, "checkpoint dir not found!")
@@ -104,29 +108,31 @@ def check_opts(opts):
     assert opts.tv_weight >= 0
     assert opts.learning_rate >= 0
 
+
 def _get_files(img_dir):
     files = list_files(img_dir)
-    return [os.path.join(img_dir,x) for x in files]
+    return [os.path.join(img_dir, x) for x in files]
 
-    
+
 def main():
     parser = build_parser()
     options = parser.parse_args()
     check_opts(options)
+    # 检查并获取参数
 
-    style_target = get_img(options.style)
+    style_target = get_img(options.style)  # 获取源风格
     if not options.slow:
         content_targets = _get_files(options.train_path)
     elif options.test:
         content_targets = [options.test]
 
     kwargs = {
-        "slow":options.slow,
-        "epochs":options.epochs,
-        "print_iterations":options.checkpoint_iterations,
-        "batch_size":options.batch_size,
-        "save_path":os.path.join(options.checkpoint_dir,'fns.ckpt'),
-        "learning_rate":options.learning_rate
+        "slow": options.slow,
+        "epochs": options.epochs,
+        "print_iterations": options.checkpoint_iterations,
+        "batch_size": options.batch_size,
+        "save_path": os.path.join(options.checkpoint_dir, 'fns.ckpt'),
+        "learning_rate": options.learning_rate
     }
 
     if options.slow:
@@ -152,16 +158,17 @@ def main():
         print('style: %s, content:%s, tv: %s' % to_print)
         if options.test:
             assert options.test_dir != False
-            preds_path = '%s/%s_%s.png' % (options.test_dir,epoch,i)
+            preds_path = '%s/%s_%s.png' % (options.test_dir, epoch, i)
             if not options.slow:
                 ckpt_dir = os.path.dirname(options.checkpoint_dir)
-                evaluate.ffwd_to_img(options.test,preds_path,
+                evaluate.ffwd_to_img(options.test, preds_path,
                                      options.checkpoint_dir)
             else:
                 save_img(preds_path, img)
     ckpt_dir = options.checkpoint_dir
     cmd_text = 'python evaluate.py --checkpoint %s ...' % ckpt_dir
     print("Training complete. For evaluation:\n    `%s`" % cmd_text)
+
 
 if __name__ == '__main__':
     main()
