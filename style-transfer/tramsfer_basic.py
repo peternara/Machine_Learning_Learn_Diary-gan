163e8d5ab8f9b97f158e06b8918814b9e04befd5
Wanqj <wqj97@126.com>
2017-06-10 17:43:20 +0800

    fixed some problem 2017-06-10

--- style-transfer/tramsfer_basic.py
+++ style-transfer/tramsfer_basic.py
@@ -77,11 +77,24 @@ def build_net(ntype, nin, nwb=None):
 
 
 def get_weight_bias(vgg_layers, i):
-    layer = vgg_layers[i][0][0][2][0]
-    weights = layer[0]
+    layers = (
+        'conv1_1', 'relu1_1', 'conv1_2', 'relu1_2', 'pool1',
+
+        'conv2_1', 'relu2_1', 'conv2_2', 'relu2_2', 'pool2',
+
+        'conv3_1', 'relu3_1', 'conv3_2', 'relu3_2', 'conv3_3',
+        'relu3_3', 'conv3_4', 'relu3_4', 'pool3',
+
+        'conv4_1', 'relu4_1', 'conv4_2', 'relu4_2', 'conv4_3',
+        'relu4_3', 'conv4_4', 'relu4_4', 'pool4',
+
+        'conv5_1', 'relu5_1', 'conv5_2', 'relu5_2', 'conv5_3',
+        'relu5_3', 'conv5_4', 'relu5_4'
+    )
+    weights, bias = vgg_layers[i][0][0][0][0]
     weights = tf.constant(weights)
-    bias = layer[1]
-    bias = tf.constant(np.reshape(bias, (bias.size)))
+    weights = tf.transpose(weights, [1, 0, 2, 3])
+    bias = tf.constant(bias.reshape(-1))
     return weights, bias
 
 
