557958bb79da4c74503271839eac006fc302992e
Wanqj <wqj97@126.com>
2017-07-03 12:29:09 +0800

    finish
    2017-07-03

--- GAN/AC-GAN/tfrecords_creater.py
+++ GAN/AC-GAN/tfrecords_creater.py
@@ -21,16 +21,24 @@ def main(arg=None):
     sess = tf.Session()
     for key, file in enumerate(files):
         print key, file
+        if tf.gfile.Exists(os.path.join(flags.checkpointDir, os.path.basename(file)) + '.tfrecords'):
+            print "{} already Exist, skip!".format(file)
+            continue
         try:
             image = scipy.misc.imread(file)
-            if not len(np.shape(image)) == 3:
+            image_shape = np.shape(image)
+            if not len(image_shape) == 3:
                 print "error: {} is bad image".format(file)
                 continue
+            else:
+                image = image[:, :, 0:3]
         except Warning, e:
-            print "warning: {}, {} has broken".format(e.message, file)
+            print "warning: {}, {} has broken will be Delete".format(e.message, file)
+            # os.remove(file)
             continue
         except Exception, e:
-            print "error: {}, {} has broken".format(e.message, file)
+            print "error: {}, {} has broken will be Delete".format(e.message, file)
+            # os.remove(file)
             continue
         else:
             image = tf.image.per_image_standardization(image)
@@ -51,6 +59,7 @@ def main(arg=None):
 
             writer.write(example.SerializeToString())
             writer.close()
+            # os.remove(file)
 
 
 if __name__ == '__main__':
